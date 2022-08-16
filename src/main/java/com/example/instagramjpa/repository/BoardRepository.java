df1c66ea033e96f27815e6c80c2a190cbcb83924
임현우 <gusdn8926@naver.com>
2022-08-16 13:45:52 +0900

    travis 테스트용 커밋푸시

--- src/main/java/com/example/instagramjpa/repository/BoardRepository.java
+++ src/main/java/com/example/instagramjpa/repository/BoardRepository.java
@@ -1,11 +1,13 @@
 package com.example.instagramjpa.repository;
 
 import com.example.instagramjpa.domain.Board;
+import com.example.instagramjpa.domain.Comment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
