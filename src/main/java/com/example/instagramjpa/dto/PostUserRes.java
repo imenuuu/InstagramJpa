d47bfcaa172fbdd35124bff6ff17c391fc775df3
임현우 <gusdn8926@naver.com>
2022-08-16 13:17:00 +0900

    travis 테스트용 커밋푸시

--- src/main/java/com/example/instagramjpa/dto/PostUserRes.java
+++ src/main/java/com/example/instagramjpa/dto/PostUserRes.java
@@ -1,5 +1,7 @@
 package com.example.instagramjpa.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +10,14 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class PostUserRes {
+    @ApiModelProperty(
+            name="userId"
+            ,example = "4"
+    )
+    @ApiParam(value = "사용자 Id")
     private Long userId;
+    @ApiParam(value = "액세스 토큰")
     private String accessToken;
+    @ApiParam(value = "리프레쉬 토큰")
     private String refreshToken;
 }
